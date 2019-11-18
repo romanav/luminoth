88e6a48d58922246f175a389304432513d4b730e
romanaverkin <romanaverkin@gmail.com>
2019-11-18 20:26:40 +0200

    Data reader created

--- TestData/DataSetReader.py
+++ TestData/DataSetReader.py
@@ -1,24 +1,17 @@
 import yaml
 
 
-
-
 class TestImage(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, image_node):
+        self.image_path = image_node['image_path']
 
 
 class DataSetReader(object):
 
-    def __init__(self, data_file):
-        self._data_file = data_file
-        with open(data_file) as data_file:
+    def __init__(self, data_file_path):
+        with open(data_file_path) as data_file:
             self._data = yaml.load(data_file)
 
     def get_images(self):
-        return [TestImage()]
-
-
-
-
+        return [TestImage(i) for i in self._data['test_data']]
--- code_unittests/DataSetReaderTest.py
+++ code_unittests/DataSetReaderTest.py
@@ -5,10 +5,13 @@ from TestData.DataSetReader import DataSetReader, TestImage
 
 @pytest.fixture
 def data_reader():
-    data_file = "mocs/test_data.json"
+    data_file = "mocs/test_data.yaml"
     return DataSetReader(data_file)
 
 
 def test_read_data(data_reader):
     images = data_reader.get_images()
     assert isinstance(images[0], TestImage)
+
+    assert images[0].image_path == "bikes.jpg"
+    assert images[1].image_path == "people.jpg"
