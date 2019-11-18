e97bb0aecd9ff1033e8073ec536ac329caaa1b78
romanaverkin <romanaverkin@gmail.com>
2019-11-19 00:59:43 +0200

    workings

---
+++ README.md
@@ -0,0 +1,34 @@
+To run this project I have utilized https://luminoth.ai/
+
+Installation packages are:
+tensorflow==1.15.0 (The latest package does not not work with luminoth)
+Dataset that was used :
+# utilizing  dataset: e1c2565b51e9 |   Faster R-CNN w/COCO | #
+
+
+This example written to show my codding abilities in python and does not represent all testing that should be done for specific component.
+Current solution based on testing dataset that framework user can extend.
+1) Data set located in resources folder
+1) test_data.yaml file define the data set :
+   * picture to run on image recognition framework
+   * expected response from the system
+
+1) Current solution has only two picture but it can be extended by adding new
+
+There is only one test in that solution that goes over data set and check if returned results from service are equal to expected.
+Solutions ignore order in witch it returned from service , but request that all values will be identical.
+For sure, in real world we need to consider some different results each time, also in sytem that learn the world all the time
+Real test should check if all expected objects will be found and coordinates of objects will be slightly different
+
+I was requested to use matplotlib, so I solve printing recognition times to the graph
+
+Project was written with TDD approach and all framework classes tested.
+In my opinion it super in important to test automation first, to be sure that our framework don't miss defect or create false alarms
+
+
+Structure:
+1) Framework unit tests located in: */unittest*
+2) System test */testing.py*
+
+# Important notes
+1) Some magic values should be extracted to the config files (didn't do it to make things simple)
--- testing.py
+++ testing.py
@@ -1,8 +1,11 @@
+import datetime
+
 import pytest
 
 from test_lib.DataAbstraction import ImageAnalysisData
 from test_lib.DataSetReader import DataSetReader
 from luminoth import Detector, read_image, vis_objects
+import matplotlib.pyplot as plt
 
 resources_folder = 'resources'
 data_file = "test_data.yaml"
@@ -19,14 +22,35 @@ def detector():
 
 
 def test_run_in_bulk_validations_from_file(data_provider, detector):
+    execution_times = []
+    execution_names = []
+
     for test_image in data_provider:
         image = read_image(test_image.image_path)
         print("Testing image: " + test_image.image_path)
-        objects = detector.predict(image)
 
-        returned_data = ImageAnalysisData(objects)
-        for i in returned_data:
-            rect = i.get_rectangle()
-            expected = test_image.get_expected_data()[rect]
-            assert expected == i
+        start_time = datetime.datetime.now()
+        detected_objects = detector.predict(image)
+        recognition_time = (datetime.datetime.now() - start_time).total_seconds()
 
+        execution_names.append(test_image.image_path)
+        execution_times.append(recognition_time)
+
+        assert_data_recognized(detected_objects, test_image)
+
+    plot_data_to_file(execution_names, execution_times)
+
+
+def plot_data_to_file(execution_names, execution_times):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(execution_names, execution_times, color='darkgreen', marker='^')
+    plt.savefig('execution_times.png')
+
+
+def assert_data_recognized(detected_objects, test_image):
+    returned_data = ImageAnalysisData(detected_objects)
+    for i in returned_data:
+        rect = i.get_rectangle()
+        expected = test_image.get_expected_data()[rect]
+        assert expected == i
