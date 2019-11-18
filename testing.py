432e57229d139a320141aa766fa7ddcdb90ce543
romanaverkin <romanaverkin@gmail.com>
2019-11-18 21:39:51 +0200

    TEST PASSING

--- testing.py
+++ testing.py
@@ -1,11 +1,10 @@
 import pytest
-import luminoth
 
 from TestData.DataSetReader import DataSetReader
+from luminoth import Detector, read_image, vis_objects
 
 resources_folder = 'resources'
 data_file = "test_data.yaml"
-from luminoth import Detector, read_image, vis_objects
 
 
 @pytest.fixture
@@ -21,7 +20,7 @@ def detector():
 def test_data_read(data_provider, detector):
     for test_image in data_provider:
         image = read_image(test_image.image_path)
-        print("Testing image: "+ test_image.image_path)
+        print("Testing image: " + test_image.image_path)
         objects = detector.predict(image)
         assert test_image.expected_data == objects
         print("passed")
